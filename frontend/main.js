fea239305ff6e06a5c3925dfa115dafdde1e8a2b
Jamie Barron <70959875+barronjamie@users.noreply.github.com>
2024-09-10 12:21:57 +1000

    Created function app and bindings

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = "https://getresumecounterjamie.azurewebsites.net/api/GetResumeCounter?code=V9r8Eutp1cAoD5byWuMbUmcCUmvn7fs9JdHTsB4fCQJRAzFuFCYulw%3D%3D"
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.")
